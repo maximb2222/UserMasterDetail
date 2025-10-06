fa54967d50ff43874a2a4b9e1255edaeaa751ddb
USER <USER@DESKTOP-VHDKGG4>
2025-10-06 20:11:37 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UserMasterDetail
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class UserViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<User> _users;
+        private User _selectedUser;
+
+        public ObservableCollection<User> Users
+        {
+            get => _users;
+            set { _users = value; OnPropertyChanged(); }
+        }
+
+        public User SelectedUser
+        {
+            get => _selectedUser;
+            set { _selectedUser = value; OnPropertyChanged(); }
+        }
+
+        public ICommand AddUserCommand { get; }
+        public ICommand DeleteUserCommand { get; }
+
+        public UserViewModel()
+        {
+            Users = new ObservableCollection<User>
+        {
+            new User { Name = "Иван Иванов", Email = "ivan@example.com", Age = 30, Department = "IT" },
+            new User { Name = "Петр Петров", Email = "petr@example.com", Age = 25, Department = "HR" },
+            new User { Name = "Сергей Сергеев", Email = "sergey@example.com", Age = 35, Department = "Finance" }
+        };
+
+            AddUserCommand = new RelayCommand(AddUser);
+            DeleteUserCommand = new RelayCommand(DeleteUser, CanDeleteUser);
+        }
+
+        private void AddUser(object obj)
+        {
+            var newUser = new User { Name = "Новый пользователь", Age = 18 };
+            Users.Add(newUser);
+            SelectedUser = newUser;
+        }
+
+        private void DeleteUser(object obj)
+        {
+            if (SelectedUser != null)
+            {
+                Users.Remove(SelectedUser);
+            }
+        }
+
+        private bool CanDeleteUser(object obj) => SelectedUser != null;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class NullToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter) => _canExecute == null || _canExecute(parameter);
+        public void Execute(object parameter) => _execute(parameter);
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+
+    public class User : INotifyPropertyChanged
+    {
+        private string _name;
+        private string _email;
+        private int _age;
+        private string _department;
+
+        public string Name
+        {
+            get => _name;
+            set { _name = value; OnPropertyChanged(); }
+        }
+
+        public string Email
+        {
+            get => _email;
+            set { _email = value; OnPropertyChanged(); }
+        }
+
+        public int Age
+        {
+            get => _age;
+            set { _age = value; OnPropertyChanged(); }
+        }
+
+        public string Department
+        {
+            get => _department;
+            set { _department = value; OnPropertyChanged(); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+}
